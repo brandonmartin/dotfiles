8e9d24517ad97bbcd87ca21e7a88e45cb4cc9665
Steve Losh <steve@stevelosh.com>
2009-12-15 20:42:27 -0500

    Update the docs to work with the new version of Markdoc.

--- docs/publish.sh
+++ docs/publish.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
-markdoc build --document-root '/hg-prompt/'
+markdoc build
 rsync --delete -az .html/ ~/src/sjl.bitbucket.org/hg-prompt
 hg -R ~/src/sjl.bitbucket.org commit -Am 'hg-prompt: Update documentation.'
 hg -R ~/src/sjl.bitbucket.org push
