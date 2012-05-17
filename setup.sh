0b5797a21897614de648579e12f212276cddcefb
Brandon Martin <b@heyomayeah.com>
2012-05-17 15:24:55 -0700

    add setup script

---
+++ setup.sh
@@ -0,0 +1,34 @@
+#!/usr/bin/env sh
+# sets up dotfiles 
+
+BASE="GIT/dotfiles"
+LINKS=".bashrc .conkyrc .gitconfig .hgrc .oh-my-zsh"
+LINKS="${LINKS} .scrotwm.conf .vim .vimrc .xinitrc"
+LINKS="${LINKS} .Xresources .zshrc"
+
+if [ -z "${HOME}" ]; then
+  echo "Error: \$HOME must be set"
+  exit 1
+fi
+
+
+for link in ${LINKS}; do
+  if [ -L "${HOME}/${link}" ]; then
+    if [ "$(readlink ${HOME}/${link})" = "${BASE}/${link}" ]; then
+      continue
+    fi
+  fi
+
+  if [ -r "${HOME}/${link}" ]; then
+    echo mv "${HOME}/${link}" "${HOME}/${link}.dfsetup_bak"
+    mv "${HOME}/${link}" "${HOME}/${link}.dfsetup_bak"
+  fi
+
+  echo ln -sf "${BASE}/${link}" "${HOME}/${link}"
+  ln -sf "${BASE}/${link}" "${HOME}/${link}"
+
+done
+
+echo "you're all set!"
+exit 0
+
